0795493de5fdcb770e5bf93c2b4934bbe0afc55a
thabungm <thabungm@gmail.com>
2024-06-10 12:36:11 +0530

    adding types

--- src/courseBatch.ts
+++ src/courseBatch.ts
@@ -1,151 +1,157 @@
-import { Document, Schema } from 'mongoose';
-import { ActiveStatus } from './common';
-import { QueryStringType } from './pagination';
-import { IUser } from './user';
-import { NewEvent } from './event';
-import { Course } from './course';
+import { Document, Schema } from 'mongoose'
+import { ActiveStatus } from './common'
+import { QueryStringType } from './pagination'
+import { IUser } from './user'
+import { NewEvent } from './event'
+import { Course } from './course'
 export enum FeedbackPurpose {
-  COURSE_BATCH = 'batch',
-  COURSE_BATCH_SESSION = 'session',
-  APP_SERVICE = 'APP_SERVICE',
+ COURSE_BATCH = 'batch',
+ COURSE_BATCH_SESSION = 'session',
+ APP_SERVICE = 'APP_SERVICE'
 }
 
 export enum EventPurpose {
-  COURSE_BATCH = 'batch',
-  COURSE_BATCH_SESSION = 'session',
+ COURSE_BATCH = 'batch',
+ COURSE_BATCH_SESSION = 'session'
 }
 
 export enum EventMode {
-  ONLINE = 'ONLINE',
-  OFFLINE = 'OFFLINE',
+ ONLINE = 'ONLINE',
+ OFFLINE = 'OFFLINE'
 }
 
 export enum CourseBatchAssnStatus {
-  PENDING = 'PENDING',
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
+ PENDING = 'PENDING',
+ ACTIVE = 'ACTIVE',
+ INACTIVE = 'INACTIVE'
 }
 export enum SessionStatus {
-  NOT_STARTED = 'NOT_STARTED',
-  IN_PROGRESS = 'IN_PROGRESS',
-  COMPLETED = 'COMPLETED',
-  CANCELLED = 'CANCELLED',
+ NOT_STARTED = 'NOT_STARTED',
+ IN_PROGRESS = 'IN_PROGRESS',
+ COMPLETED = 'COMPLETED',
+ CANCELLED = 'CANCELLED'
 }
 
 export enum EventStatus {
-  NOT_STARTED = 'NOT_STARTED',
-  IN_PROGRESS = 'IN_PROGRESS',
-  COMPLETED = 'COMPLETED',
-  CANCELLED = 'CANCELLED',
+ NOT_STARTED = 'NOT_STARTED',
+ IN_PROGRESS = 'IN_PROGRESS',
+ COMPLETED = 'COMPLETED',
+ CANCELLED = 'CANCELLED'
 }
 
 export enum CourseBatchStatus {
-  NOT_STARTED = 'NOT_STARTED',
-  IN_PROGRESS = 'IN_PROGRESS',
-  COMPLETED = 'COMPLETED',
-  CANCELLED = 'CANCELLED',
+ NOT_STARTED = 'NOT_STARTED',
+ IN_PROGRESS = 'IN_PROGRESS',
+ COMPLETED = 'COMPLETED',
+ CANCELLED = 'CANCELLED'
 }
 
 export enum CourseBatchTrackerStatus {
-  NOT_STARTED = 'NOT_STARTED',
-  IN_PROGRESS = 'IN_PROGRESS',
-  COMPLETED = 'COMPLETED',
+ NOT_STARTED = 'NOT_STARTED',
+ IN_PROGRESS = 'IN_PROGRESS',
+ COMPLETED = 'COMPLETED'
 }
 
 export enum SessionFrequency {
-  DAILY = 'DAILY',
-  WEEKLY = 'WEEKLY',
-  WEEKENDS = 'WEEKENDS',
-  ALTERNATE_DAYS = 'ALTERNATE_DAYS',
-  WEEKDAYS = 'WEEKDAYS',
+ DAILY = 'DAILY',
+ WEEKLY = 'WEEKLY',
+ WEEKENDS = 'WEEKENDS',
+ ALTERNATE_DAYS = 'ALTERNATE_DAYS',
+ WEEKDAYS = 'WEEKDAYS'
 }
 
 interface CourseProgressSchema {
-  courseId: Schema.Types.ObjectId;
-  chapters: {
-    _id?: Schema.Types.ObjectId;
+ courseId: Schema.Types.ObjectId
+ chapters: {
+  _id?: Schema.Types.ObjectId
 
-    name: string;
-    topics: {
-      _id?: Schema.Types.ObjectId;
+  name: string
+  topics: {
+   _id?: Schema.Types.ObjectId
 
-      name: string;
-      status: CourseBatchTrackerStatus;
-    }[];
-    status: CourseBatchTrackerStatus;
-  }[];
-  status: CourseBatchTrackerStatus;
+   name: string
+   status: CourseBatchTrackerStatus
+  }[]
+  status: CourseBatchTrackerStatus
+ }[]
+ status: CourseBatchTrackerStatus
 }
 export interface ICourseBatch extends Document {
-  name: string;
-  code: string;
-  courseId: Schema.Types.ObjectId;
-  instructors?: Schema.Types.ObjectId[];
-  currentInstructor?: Schema.Types.ObjectId;
-  event?: Schema.Types.ObjectId;
-  courseProgress?: CourseProgressSchema;
-  status?: CourseBatchStatus;
+ name: string
+ code: string
+ courseId: Schema.Types.ObjectId
+ instructors?: Schema.Types.ObjectId[]
+ currentInstructor?: Schema.Types.ObjectId
+ event?: Schema.Types.ObjectId
+ courseProgress?: CourseProgressSchema
+ status?: CourseBatchStatus
+ stats?: {
+  totalStudents: number
+  completedStudents: number
+  totalSessions: number
+  completedSessions: number
+ }
 }
 
 export interface NewBatch extends Omit<ICourseBatch, keyof Document | 'event'> {
-  event: NewEvent;
+ event: NewEvent
 }
 
 export interface IStudentBatchAssociation extends Document {
-  user: Schema.Types.ObjectId;
-  batch: Schema.Types.ObjectId;
-  status?: CourseBatchAssnStatus;
+ user: Schema.Types.ObjectId
+ batch: Schema.Types.ObjectId
+ status?: CourseBatchAssnStatus
 }
 
 export interface ICourseBatchSessionAttendance {
-  _id?: Schema.Types.ObjectId;
-  user: Schema.Types.ObjectId;
-  session: Schema.Types.ObjectId;
+ _id?: Schema.Types.ObjectId
+ user: Schema.Types.ObjectId
+ session: Schema.Types.ObjectId
 }
 
 export interface ICourseBatchSession extends Document {
-  slNo?: number;
-  name: string;
-  batchId: Schema.Types.ObjectId;
-  duration: number;
-  status?: SessionStatus;
-  startDateTime: Date;
-  endDateTime?: Date;
-  teacherId: Schema.Types.ObjectId;
-  feedback?: Schema.Types.ObjectId[];
-  notes?: string;
+ slNo?: number
+ name: string
+ batchId: Schema.Types.ObjectId
+ duration: number
+ status?: SessionStatus
+ startDateTime: Date
+ endDateTime?: Date
+ teacherId: Schema.Types.ObjectId
+ feedback?: Schema.Types.ObjectId[]
+ notes?: string
 }
 
 export interface ICourseBatchSessionClient extends ICourseBatch {
-  course: Course;
+ course: Course
 }
 
 export interface NewCourseBatchSession {
-  batchId: string;
+ batchId: string
 }
 
-export type CourseBatchSessionType = Omit<ICourseBatchSession, keyof Document>;
+export type CourseBatchSessionType = Omit<ICourseBatchSession, keyof Document>
 export interface IFeedback extends Document {
-  user: Schema.Types.ObjectId;
-  batchId?: Schema.Types.ObjectId;
-  sessionId?: Schema.Types.ObjectId;
-  purpose: FeedbackPurpose;
-  comment?: string;
-  rating?: number;
-  feedbackByTopics?: {
-    topic: Schema.Types.ObjectId;
-    rating?: number;
-    isChecked?: boolean;
-  }[];
+ user: Schema.Types.ObjectId
+ batchId?: Schema.Types.ObjectId
+ sessionId?: Schema.Types.ObjectId
+ purpose: FeedbackPurpose
+ comment?: string
+ rating?: number
+ feedbackByTopics?: {
+  topic: Schema.Types.ObjectId
+  rating?: number
+  isChecked?: boolean
+ }[]
 }
 
 export interface CourseProgressUpdateType {
-  batchId: string;
-  chapterId?: string;
-  topicId?: string;
-  status?: CourseBatchTrackerStatus;
+ batchId: string
+ chapterId?: string
+ topicId?: string
+ status?: CourseBatchTrackerStatus
 }
-export type NewFeedback = Omit<IFeedback, keyof Document>;
+export type NewFeedback = Omit<IFeedback, keyof Document>
 
 /* const newFeedback: NewFeedback = {
  
@@ -162,70 +168,70 @@ export type NewFeedback = Omit<IFeedback, keyof Document>;
 }; */
 
 export interface IFeedbackQuery {
-  query?: {
-    user?: string;
-    batch?: string;
-    session?: string;
-    populate?: string;
-    populateKeys?: string;
-  } & QueryStringType;
+ query?: {
+  user?: string
+  batch?: string
+  session?: string
+  populate?: string
+  populateKeys?: string
+ } & QueryStringType
 }
 
 //this event can be mapped to calender to present (in the UI)
 
 export interface IStudentBatchAssocQuery {
-  query?: {
-    user?: string;
-    batch?: string;
-    populate?: string;
-    populateKeys?: string;
-  } & QueryStringType;
+ query?: {
+  user?: string
+  batch?: string
+  populate?: string
+  populateKeys?: string
+ } & QueryStringType
 }
 
 export interface ICourseBatchTracker extends Document {
-  batch: Schema.Types.ObjectId;
-  subject: Schema.Types.ObjectId;
-  chapter: Schema.Types.ObjectId;
-  topic: Schema.Types.ObjectId;
-  course: {
-    courseId: Schema.Types.ObjectId;
-    subjects: {
-      subjectId: Schema.Types.ObjectId;
-      chapters: {
-        name: string;
-        topics: {
-          name: string;
-          status: CourseBatchTrackerStatus;
-        }[];
-        status: CourseBatchTrackerStatus;
-      }[];
-      status: CourseBatchTrackerStatus;
-    }[];
-    status: CourseBatchTrackerStatus;
-  };
-  status: CourseBatchTrackerStatus;
-  stats: {
-    totalChapters: number;
-    completedChapters: number;
-    ratings: number;
-    completedSessions: number;
-  };
-  instructor: IUser;
-  startDate: Date;
-  endDate: Date;
+ batch: Schema.Types.ObjectId
+ subject: Schema.Types.ObjectId
+ chapter: Schema.Types.ObjectId
+ topic: Schema.Types.ObjectId
+ course: {
+  courseId: Schema.Types.ObjectId
+  subjects: {
+   subjectId: Schema.Types.ObjectId
+   chapters: {
+    name: string
+    topics: {
+     name: string
+     status: CourseBatchTrackerStatus
+    }[]
+    status: CourseBatchTrackerStatus
+   }[]
+   status: CourseBatchTrackerStatus
+  }[]
+  status: CourseBatchTrackerStatus
+ }
+ status: CourseBatchTrackerStatus
+ stats: {
+  totalChapters: number
+  completedChapters: number
+  ratings: number
+  completedSessions: number
+ }
+ instructor: IUser
+ startDate: Date
+ endDate: Date
 }
 
 export interface NewBatchAssnBodyType {
-  emails?: string[];
-  studentIds?: Schema.Types.ObjectId[];
-  batchId: Schema.Types.ObjectId;
+ emails?: string[]
+ studentIds?: Schema.Types.ObjectId[]
+ batchId: Schema.Types.ObjectId
 }
 
 export interface UpdateCourseBatchTrackerInput {
-  batchId: string;
-  courseId: string;
-  subjectId: string;
-  chapterName: string;
-  topicName: string;
-  completed: boolean;
+ batchId: string
+ courseId: string
+ subjectId: string
+ chapterName: string
+ topicName: string
+ completed: boolean
 }
