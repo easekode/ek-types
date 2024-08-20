380ee1744b20c5947ce6cdffd1ac16935c579395
easekode <easekode22@gmail.com>
2024-08-20 10:50:32 +0530

    adding feedbacktype

--- src/feedback.ts
+++ src/feedback.ts
@@ -1,5 +1,13 @@
 export interface FeedbackTopic extends Document {
-  name: string;
+ name: string
+ feedbackType: FeedbackType
+ active: boolean
 }
 
-export type NewFeedbackTopic = Omit<FeedbackTopic, keyof Document>;
+export type NewFeedbackTopic = Omit<FeedbackTopic, keyof Document>
+
+export enum FeedbackType {
+ RATE = 'RATE',
+ YES_NO = 'YES_NO',
+ DESCRIPTIVE = 'DESCRIPTIVE'
+}
