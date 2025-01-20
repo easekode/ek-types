fcb08d4f9ba6809916f31c433a69cc6ad9c2a9dd
angamba <angamba@refixd.com>
2025-01-20 16:07:30 +0530

    composite type update

--- src/composite.ts
+++ src/composite.ts
@@ -1,5 +1,7 @@
 import { CoursesComposite } from './course'
+import { BatchComposite } from './courseBatch'
 
 export interface StudentCompositeData {
  courses: CoursesComposite[]
+ batches: BatchComposite[]
 }
