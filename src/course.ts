3102ab38f1500a75177ca978f7ef0bc6c851364d
angamba <angamba@refixd.com>
2025-01-27 19:13:50 +0530

    course filter type fix

--- src/course.ts
+++ src/course.ts
@@ -189,9 +189,8 @@ export type GenerateCourseResult = Partial<
  >
 >
 
-export interface CourseFilter extends CompanyIdFilter {
- status?: CourseStatus
-}
+const CourseFilterSchema = CourseSchema.partial() //.extend({})
+export type CourseFilter = z.infer<typeof CourseFilterSchema>
 
 export interface CoursesComposite {
  courseId: string
