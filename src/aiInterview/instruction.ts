b6fd088069c244cdfed4cce54149ac23b91fcb74
unknown <thoibasingha637@gmail.com>
2025-02-27 08:49:51 +0530

    added instruction to match api response

--- src/aiInterview/instruction.ts
+++ src/aiInterview/instruction.ts
@@ -1,23 +1,25 @@
 export enum ContentType {
- DYNAMIC = 'dynamic',
- STATIC = 'static'
+  DYNAMIC = 'dynamic',
+  STATIC = 'static',
 }
 
 export interface Instruction {
- title: string
- description: { content: string; type?: ContentType }[]
+  title: string;
+  description: { content: string; type?: ContentType }[];
 }
 
 export interface InterviewInstructions {
- meta: {
-  title: string
-  description: string
- }
- instructions: Instruction[]
+  instruction: {
+    meta: {
+      title: string;
+      description: string;
+    };
+    instructions: Instruction[];
+  };
 }
 
 export interface InterviewInstructionInput {
- duration: number
- totalQuestion: number
- contactInformation: string
+  duration: number;
+  totalQuestion: number;
+  contactInformation: string;
 }
