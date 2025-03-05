bd3ccaf193c551a3adea619086aa9b97a62263d2
angamba <angamba@refixd.com>
2025-03-05 13:44:09 +0530

    conflict resolve

--- src/aiInterview/InterviewQuestion.ts
+++ src/aiInterview/InterviewQuestion.ts
@@ -5,45 +5,46 @@ import { Document } from 'mongoose'
 import { QuestionType } from '../exam'
 
 export enum InterviewQuestionStatus {
- DRAFT = 'DRAFT',
- PUBLISHED = 'PUBLISHED',
- IN_PROGRESS = 'PUBLISHED',
- COMPLETED = 'COMPLETED',
+  DRAFT = 'DRAFT',
+  PUBLISHED = 'PUBLISHED',
+  IN_PROGRESS = 'PUBLISHED',
+  COMPLETED = 'COMPLETED',
  UNDER_REVIEW = 'UNDER_REVIEW'
 }
 
 export const InterviewQuestionSchema = z.object({
- title: z.string().min(1),
- problemSnippet: z.string().optional(),
- status: z.nativeEnum(InterviewQuestionStatus),
- timesUsed: z.number().optional(),
- experienceRange: z
-  .object({
-   min: z.number().min(0),
+  title: z.string().min(1),
+  problemSnippet: z.string().optional(),
+  status: z.nativeEnum(InterviewQuestionStatus),
+  timesUsed: z.number().optional(),
+  experienceRange: z
+    .object({
+      min: z.number().min(0),
    max: z.number().min(0)
-  })
-  .refine((data) => data.min <= data.max, {
+    })
+    .refine((data) => data.min <= data.max, {
    message: 'min should be less than or equal to max'
-  }),
- questions: z.array(QuestionAnswerSchema),
- totalQuestions: z.number().optional(),
- jobId: ObjectIdOrStringId
+    }),
+  questions: z.array(QuestionAnswerSchema),
+  totalQuestions: z.number().optional(),
+  jobId: ObjectIdOrStringId
 })
 
 export const UpdateInterviewQuestionSchema = InterviewQuestionSchema.partial()
- .omit({
-  timesUsed: true,
-  totalQuestions: true
- })
+  .omit({
+    timesUsed: true,
+    totalQuestions: true,
+    jobId: true,
+  })
  .strict()
 
 export const NewInterviewQuestionReqSchema = InterviewQuestionSchema.omit({
- title: true,
- status: true,
- timesUsed: true,
+  title: true,
+  status: true,
+  timesUsed: true,
  questions: true
 }).extend({
- prompt: z.string(),
+  prompt: z.string(),
  questionSet: z.number()
 })
 export type NewInterviewQuestionReq = z.infer<typeof NewInterviewQuestionReqSchema>
@@ -54,16 +55,16 @@ export type UpdateInterviewQuestion = z.infer<typeof UpdateInterviewQuestionSche
 this is the schema for the interview question that will be asked by AI to the user
 */
 export const AiInterviewQuestionSchema = z.object({
- question: z.string(),
- problemSnippet: z.string().optional(),
- choices: z.array(z.string()).optional(),
+  question: z.string(),
+  problemSnippet: z.string().optional(),
+  choices: z.array(z.string()).optional(),
  questionType: z.nativeEnum(QuestionType)
 })
 
 export type AiInterviewQuestion = z.infer<typeof AiInterviewQuestionSchema>
 
 export const AiInterviewResponseSchema = z.object({
- buffer: z.string(),
+  buffer: z.string(),
  interviewQuestion: AiInterviewQuestionSchema
 })
 export type AiInterviewResponse = z.infer<typeof AiInterviewResponseSchema>
