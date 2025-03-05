46a27861048b0367ac5d981574324fa675f54710
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2025-03-05 16:58:14 +0530

    adding status on question-set

--- src/aiInterview/QuestionAnswer.ts
+++ src/aiInterview/QuestionAnswer.ts
@@ -1,25 +1,27 @@
-import { z } from 'zod'
-import { QuestionType } from '../exam'
+import { z } from 'zod';
+import { QuestionType } from '../exam';
+import { InterviewQuestionStatus } from './InterviewQuestion';
 
 export const QuestionAnswerSchema = z.object({
- question: z.string(),
- type: z.nativeEnum(QuestionType),
- choices: z.array(z.string()).optional(),
- answer: z.array(z.string())
-})
+  question: z.string(),
+  type: z.nativeEnum(QuestionType),
+  choices: z.array(z.string()).optional(),
+  answer: z.array(z.string()),
+});
 
-export type NewQuestionAnswer = z.infer<typeof QuestionAnswerSchema>
+export type NewQuestionAnswer = z.infer<typeof QuestionAnswerSchema>;
 
 export const QuestionSetSchema = z.object({
- title: z.string(),
- questions: z.array(QuestionAnswerSchema)
-})
+  title: z.string(),
+  questions: z.array(QuestionAnswerSchema),
+  status: z.nativeEnum(InterviewQuestionStatus).optional(),
+});
 
-export type NewQuestionSet = z.infer<typeof QuestionSetSchema>
+export type NewQuestionSet = z.infer<typeof QuestionSetSchema>;
 export const NewQuestionSetsSchema = z.object({
- questionSets: z.array(QuestionSetSchema)
-})
-export type NewQuestionSets = z.infer<typeof NewQuestionSetsSchema>
+  questionSets: z.array(QuestionSetSchema),
+});
+export type NewQuestionSets = z.infer<typeof NewQuestionSetsSchema>;
 
 /* export const NewQuestionSetJsonSchema = {
  $schema: 'http://json-schema.org/draft-07/schema#',
